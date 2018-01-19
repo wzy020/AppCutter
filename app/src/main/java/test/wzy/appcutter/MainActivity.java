3b061a2dc45345f5f14fa268e5cb74bc904009d5
wangziyue <wangziyue@bitkan.com>
2018-01-19 14:48:55 +0800

    create project.

---
+++ app/src/main/java/test/wzy/appcutter/MainActivity.java
@@ -0,0 +1,13 @@
+package test.wzy.appcutter;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
